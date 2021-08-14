0e9582bf53642d25e84d7bbfe7baf0ef30417a74
Daniel Dennis <dankatdennis@gmail.com>
2021-08-14 08:07:46 +0300

    - created a simple server with vite SSR mode

---
+++ server.ts
@@ -0,0 +1,28 @@
+import express from 'express'
+import { createServer as createViteServer } from 'vite'
+import { serverRenderRoute } from './riverside/ssr/serverRenderRoute';
+
+
+// Let's create a basic express app
+
+async function createServer() {
+
+  const app = express();
+
+  // set vite to be on SSR Mode by creating a middleware
+  const vite = await createViteServer({
+    server: { middlewareMode: 'ssr' }
+  });
+
+  //calling the said SSR middleware
+  app.use(vite.middlewares);
+
+  // when a page is requested, call our SSR method
+  app.use('*', serverRenderRoute({ vite }));
+
+  // start the server
+  app.listen(3000, () => console.log("Listening on :3000"));
+
+}
+
+createServer();
