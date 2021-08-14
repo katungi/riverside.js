83e6869d862c91264de0aab3c871919163413a63
Daniel Dennis <dankatdennis@gmail.com>
2021-08-14 07:59:08 +0300

    - added vite configs

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import reactRefresh from '@vitejs/plugin-react-refresh'
+
+export default {
+  plugins: [reactRefresh()],
+  esbuild: {
+    // will inject the React import statement to the JSX files
+    jsxInject: `import React from react;`,
+  },
+  build: {
+    minify: false,
+  }
+}
